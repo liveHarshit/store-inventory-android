3688567bbcd2388a120ae1410b4e885eb4884c54
liveHarshit <live.harshit@hotmail.com>
2018-06-12 13:43:42 +0530

    Add image from gallery and user permission

--- app/src/main/java/com/example/liveharshit/storeinventory/CatalogActivity.java
+++ app/src/main/java/com/example/liveharshit/storeinventory/CatalogActivity.java
@@ -1,13 +1,51 @@
 package com.example.liveharshit.storeinventory;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class CatalogActivity extends AppCompatActivity {
+    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        if(Build.VERSION.SDK_INT>=23) {
+
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need to read the contacts
+                }
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant that should be quite unique
+
+            }
+        }
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        ListView listView = (ListView)findViewById(R.id.list);
     }
 }
