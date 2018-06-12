9832d40c6fa8062da422203626092707637bc8ae
liveHarshit <live.harshit@hotmail.com>
2018-06-12 15:19:53 +0530

    implement database helper class

--- app/src/main/java/com/example/liveharshit/storeinventory/data/StoreContract.java
+++ app/src/main/java/com/example/liveharshit/storeinventory/data/StoreContract.java
@@ -1,4 +1,21 @@
 package com.example.liveharshit.storeinventory.data;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class StoreContract {
+    public static final String CONTENT_AUTHORITY = "com.example.liveharshit.storeinventory";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private StoreContract () {}
+    public static final class StoreEntry implements BaseColumns {
+        public static final String TABLE_NAME = "products";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_IMAGE = "image";
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_AVAILABLE_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_CATEGORY = "category";
+        public static final Uri CONTENT_URI  = Uri.withAppendedPath(BASE_CONTENT_URI,TABLE_NAME);
+    }
 }
--- app/src/main/java/com/example/liveharshit/storeinventory/data/StoreDbHelper.java
+++ app/src/main/java/com/example/liveharshit/storeinventory/data/StoreDbHelper.java
@@ -1,4 +1,35 @@
 package com.example.liveharshit.storeinventory.data;
 
-public class StoreDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.liveharshit.storeinventory.data.StoreContract.StoreEntry;
+
+public class StoreDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "product.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public StoreDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PRODUCT_TABLE = "CREATE TABLE "+ StoreEntry.TABLE_NAME + "(" +
+                StoreEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+                StoreEntry.COLUMN_PRODUCT_NAME + "TEXT NOT NULL," +
+                StoreEntry.COLUMN_AVAILABLE_QUANTITY  + "INTEGER NOT NULL," +
+                StoreEntry.COLUMN_PRODUCT_PRICE + "INTEGER NOT NULL," +
+                StoreEntry.COLUMN_PRODUCT_CATEGORY + "INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_PRODUCT_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+    }
 }
